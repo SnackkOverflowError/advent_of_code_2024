9bdd7a44210f9bf7e69b152db41e39cdfc1cee40
Aaditya Chaudhary <bluebird1339@gmail.com>
2024-12-07 22:26:25 -0500

    day 2 part 2

--- src/day2/day2.clj
+++ src/day2/day2.clj
@@ -25,8 +25,8 @@
          (fn [acc [index value]] 
            ; have the current value, get the one previous
            (if (not= index 0) 
-              (if  (are_levels_unsafe value (nth report (dec index)) is_inc) 
-                (do (println "unsafe" value (nth report (dec index))) (reduced false)) (do (println "safe" value (nth report (dec index))) ))
+              (if (are_levels_unsafe value (nth report (dec index)) is_inc) 
+                (do (println "unsafe" value (nth report (dec index))) (reduced false)) (println "safe" value (nth report (dec index)) ))
                ()
          )) nil (map-indexed vector report) )]
     (if (nil? safe) 
@@ -35,11 +35,63 @@
     )
 ))
 
+
+; return 1 for safe, 0 for unsafe
+(defn is_report_safe_part_2 [report]
+  ;(println "report" report)
+  ;(println "is increment" (< (nth report 0) (nth report 1)))
+  (let [is_inc (< (nth report 0) (nth report 1))  
+        safe (reduce 
+         (fn [acc [index value]] 
+           ; have the current value, get the one previous
+           (if (not= index 0) 
+              (if (are_levels_unsafe value (nth report (dec index)) is_inc) 
+                (do 
+               ;   (println "unsafe" value (nth report (dec index))) 
+                  (reduced [0 index (dec index)])
+                ) 
+                (
+    ;             println "safe" value (nth report (dec index)) 
+                )
+              )
+           ()
+         )) nil (map-indexed vector report) )]
+    (if (or (nil? safe ) (empty? safe)) 
+      (do 
+        ;(println "safe is nil" safe)
+        [1]
+       ) 
+      (do
+      ;(println "safe" safe)
+      safe
+      )
+    )
+))
+
+(defn remove-index [arr index] 
+  (concat (take index arr) (drop (inc index) arr))
+)
+
+(defn part_2_wrapper [report]
+  (let [res (is_report_safe_part_2 report)]
+  (if (= (get res 0) 0) 
+    (reduce (fn [_ index] 
+            (let [res (is_report_safe_part_2 (remove-index report index))]
+            (if (= 1 (get res 0))
+              (reduced 1)
+              0
+             ))
+          ) 0 (range (count report))) 
+  1
+  )
+
+))
+
 (defn part_1 [input] 
   ;(println "input" input)
   ; for each report check if its safe. The value returned 
   ; will be the number of safe reports
-  (reduce + (map is_report_safe input))
+  (reduce + (map part_2_wrapper input))
   ;(is_report_safe [50 49 47 46 48])
 )
 
